61b5418cf97e1d2ff81f5cc53f28482053beabde
Mike Basdeo <mikebasdeo@gmail.com>
2020-05-24 12:49:48 -0400

    Router and SubRoutes exercise working

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,19 @@ import { Router } from 'express'
 
 const router = Router()
 
+const controller = (req, res) => {
+  res.send(418)
+}
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .delete(controller)
+  .put(controller)
+
 export default router
