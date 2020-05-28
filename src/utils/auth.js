8d999c4dd5e7e2bddfe5251581424cd7c0ef3843
Mike Basdeo <mikebasdeo@gmail.com>
2020-05-28 15:46:33 -0400

    3/4 done lesson 5. JWT Authentication

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,9 +16,52 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  try {
+    // create user
+    const myUser = await User.create(req.body)
 
-export const signin = async (req, res) => {}
+    // create token
+    const myToken = await newToken(myUser)
+
+    return res.status(201).send({ token: myToken })
+  } catch (error) {
+    console.log(error)
+    return res.status(400).send({ message: `Email and password required` })
+  }
+}
+
+export const signin = async (req, res) => {
+  // check for valid input
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .send({ message: 'Please enter a valid email and password' })
+  }
+
+  // check user exists
+  const myUser = await User.findOne({ email: req.body.email }).exec()
+
+  if (!myUser) {
+    return res.status(401).send({ message: 'No User Found' })
+  }
+
+  // check for correct password
+  try {
+    const match = await myUser.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'Wrong Password' })
+    }
+
+    // create a token
+    const myToken = await newToken(myUser)
+
+    return res.status(201).send({ token: myToken })
+  } catch (error) {
+    console.log(error)
+    return res.status(401).send({ message: 'Wrong Password' })
+  }
+}
 
 export const protect = async (req, res, next) => {
   next()
